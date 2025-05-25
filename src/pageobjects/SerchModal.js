da123ed142de2a0d4b8311c680a3f6f9d8475200
bors4 <sasha.belik89@gmail.com>
2025-05-26 00:05:04 +0300

    доработан шаг поиска товаров

--- src/pageobjects/SerchModal.js
+++ src/pageobjects/SerchModal.js
@@ -4,6 +4,7 @@ class SearchModal {
             'чекбокс К сравнению': '//input[contains(@data-bind, "compare")]',
             'Ничего не найдено': '//div[@class="search__bar"]//div/following-sibling::div[contains(text(), "Ничего не найдено")]',//'//div[@id="search-page"]',//'//div[@class="search__bar"]//div/following-sibling::div[contains(text(), "Ничего не найдено")]',
             'закрыть окно поиска': '//*/span[@class="search__close"]',
+            'поле поиска': '//*/input[@placeholder="Поиск"]'
         }
     }
 
